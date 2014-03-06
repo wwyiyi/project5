2e287bedc44ddb292455f8c386e74014caaff626
Yao Xiao <wwyiyi921111@gmail.com>
2014-03-06 00:17:15 -0600

    recover previous files

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -95,6 +95,16 @@ describe User do
     before { @user.password = @user.password_confirmation = "a" * 5 }
     it { should be_invalid }
   end
+
+  describe "email address with mixed case" do  
+    let(:mixed_case_email) { "Foo@ExAPMle.CoM" }
+  
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      expect(@user.reload.email).to eq mixed_case_email.downcase
+    end
+  end
 end
 
              
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,35 +9,25 @@ describe "Static pages" do
   describe "Home page" do
     before { visit root_path }
 
-    it { should have_content('Sample App') }
-    it { should have_title("Ruby on Rails Tutorial Sample App") }
+    it { should have_selector('h1', text: 'Welcome to the Sample App') }
+    it { should have_title(full_title('')) }
     it { should_not have_title('| Home') }
   end
 
   describe "Help page" do
 
-    it "should have the content 'Help'" do
-      visit help_path
-      expect(page).to have_content('Help')
-    end
+    before { visit help_path }
 
-    it "should have the title 'Help'" do
-      visit help_path
-      expect(page).to have_title("#{base_title} | Help")
-    end
+    it { should have_selector('h1', text: 'Help') }
+    it { should have_title(full_title('Help')) }
   end
 
   describe "About page" do
 
-    it "should have the content 'About Us'" do
-      visit about_path
-      expect(page).to have_content('About Us')
-    end
+    before { visit about_path }
 
-    it "should have the title 'About Us'" do
-      visit about_path
-      expect(page).to have_title("#{base_title} | About Us")
-    end
+    it { should have_selector('h1', text: 'About Us') }
+    it { should have_title(full_title('About Us')) }
   end
 
    describe "Contact page" do
@@ -45,5 +35,20 @@ describe "Static pages" do
 
     it { should have_selector('h1', text: 'Contact') }
     it { should have_title(full_title('Contact')) }
+  end
+  
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    expect(page).to have_title(full_title('About Us'))
+    click_link "Help"
+    expect(page).to have_title(full_title('Help'))
+    click_link "Contact"
+    expect(page).to have_title(full_title('Contact'))
+    click_link "Home"
+    click_link "Sign up now!"
+    expect(page).to have_title(full_title('Sign up'))
+    click_link "sample app"
+    expect(page).to have_title(full_title(''))
   end
 end
