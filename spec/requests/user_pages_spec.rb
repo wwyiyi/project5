791683d9598785d5d7bd56b8c1607d4d9cacc233
Yao Xiao <wwyiyi921111@gmail.com>
2014-03-10 21:49:15 -0500

    7.6.1 test passes

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -33,6 +33,35 @@ describe "User pages" do
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
       end
+      
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by(email: 'user@example.com') }
+
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+      end
+      
+    end
+    
+    describe "with invalid information" do
+      before do
+        fill_in "Name",         with: "Foo Bar"
+        fill_in "Email",        with: "foo@invalid"
+        fill_in "Password",     with: ""
+        fill_in "Confirmation", with: ""
+      end
+      
+      it "should not create a user" do
+        expect { click_button submit }.not_to change(User, :count)
+      end
+      
+      describe "after submission" do
+        before { click_button submit }
+
+        it { should have_title('Sign up') }
+        it { should have_content('error') }
+      end
     end
 
     it { should have_content('Sign up') }
