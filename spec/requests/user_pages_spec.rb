ca8731f054655bd2adebf7054628deef6ece9e83
Yao Xiao <wwyiyi921111@gmail.com>
2014-04-01 16:39:13 -0500

    8.5.2

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -13,6 +13,7 @@ describe "User pages" do
   end
 
   describe "signup page" do
+
     before { visit signup_path }
     
     let(:submit) { "Create my account" }
@@ -23,12 +24,11 @@ describe "User pages" do
     end
 
     describe "with valid information" do
-      before do
-        fill_in "Name",         with: "Example User"
-        fill_in "Email",        with: "user@example.com"
-        fill_in "Password",     with: "foobar"
-        fill_in "Confirmation", with: "foobar"
-      end
+      #let(:user) { FactoryGirl.create(:user) }
+      before { valid_signup }
+
+      
+      #before { valid_signup(user) }
 
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
@@ -53,12 +53,7 @@ describe "User pages" do
     end
     
     describe "with invalid information" do
-      before do
-        fill_in "Name",         with: "Foo Bar"
-        fill_in "Email",        with: "foo@invalid"
-        fill_in "Password",     with: ""
-        fill_in "Confirmation", with: ""
-      end
+      before { invalid_signup }
       
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -14,6 +14,20 @@ def valid_signin(user)
   click_button "Sign in"
 end
 
+def valid_signup
+  fill_in "Name",         with: "Example User"
+  fill_in "Email",        with: "user@example.com"
+  fill_in "Password",     with: "foobar"
+  fill_in "Confirmation", with: "foobar"
+end
+
+def invalid_signup
+  fill_in "Name",         with: "Foo Bar"
+  fill_in "Email",        with: "foo@invalid"
+  fill_in "Password",     with: ""
+  fill_in "Confirmation", with: ""
+end
+
 RSpec::Matchers.define :have_error_message do |message|
   match do |page|
     expect(page).to have_selector('div.alert.alert-error', text: message)
