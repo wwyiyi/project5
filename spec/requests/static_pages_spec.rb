3ccf1ecdf0e638519d989c274ed2e11caf1b80a6
Yao Xiao <wwyiyi921111@gmail.com>
2014-04-25 01:00:40 -0500

    finish 11.2

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -13,6 +13,18 @@ describe "Static pages" do
     it { should have_title(full_title('')) }
     it { should_not have_title('| Home') }
     
+    describe "for signed-in users with one micropost" do
+      let(:user) { FactoryGirl.create(:user) }
+      before do
+        FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
+        sign_in user
+        visit root_path
+      end
+        it { should have_content('micropost')}
+        it { should have_content(user.microposts.count)}
+        specify { expect(user.microposts.count).to  eq 1} 
+    end  
+    
     describe "for signed-in users" do
       let(:user) { FactoryGirl.create(:user) }
       before do
@@ -33,20 +45,18 @@ describe "Static pages" do
           expect(page).to have_selector("li##{item.id}", text: item.content)
         end
       end
-    end
+      
+      describe "follower/following counts" do
+        let(:other_user) { FactoryGirl.create(:user) }
+        before do
+          other_user.follow!(user)
+          visit root_path
+        end
 
-
-    describe "for signed-in users with one micropost" do
-      let(:user) { FactoryGirl.create(:user) }
-      before do
-        FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
-        sign_in user
-        visit root_path
+        it { should have_link("0 following", href: following_user_path(user)) }
+        it { should have_link("1 followers", href: followers_user_path(user)) }
       end
-        it { should have_content('micropost')}
-        it { should have_content(user.microposts.count)}
-        specify { expect(user.microposts.count).to  eq 1} 
-    end  
+    end
   end
 
   describe "Help page" do
