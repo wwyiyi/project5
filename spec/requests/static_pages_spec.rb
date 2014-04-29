7f8400fecb8d3ea5ab865ce02537f0a005260696
lanxi <lhuang38@wisc.edu>
2014-04-29 11:58:00 -0500

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,34 +1,38 @@
 require 'spec_helper'
 
 describe "StaticPages" do
-  describe "Home page" do
+  subject { page }
 
-    it "should have the content 'Home'" do
-      visit root_path
-      expect(page).to have_content('Home')
-    end
-
-    it "should have the base title" do
-      visit root_path
-      expect(page).to have_title("Project5")
-    end
-    it "should not have a custom page title" do
-      visit '/static_pages/home'
-      expect(page).not_to have_title('| Home')
-    end
-    
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1', text: heading) }
+    it { should have_title(full_title(page_title)) }
   end
-  
+
+  describe "Home page" do
+    before { visit root_path }
+    let(:heading)    { 'Home' }
+    let(:page_title) { '' }
+
+    it_should_behave_like "all static pages"
+    it { should_not have_title('| Home') }
+  end
+
   describe "Help page" do
+    before { visit help_path }
+    let(:heading)    { 'Help' }
+    let(:page_title) { 'Help' }
 
-    it "should have the content 'Help'" do
-      visit help_path
-      expect(page).to have_content('Help')
-    end
+    it_should_behave_like "all static pages"
+  end
 
-    it "should have the title 'Help'" do
-      visit help_path
-      expect(page).to have_title("Project5 | Help")
-    end
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "Help"
+    expect(page).to have_content("Help")
+    click_link "Home"
+    click_link "Sign up now!"
+    expect(page).to have_content("Sign up")
+    click_link "Project5"
+    expect(page).to have_content("Home")
   end
 end
