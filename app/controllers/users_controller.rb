630663f36100d73948a12bf9948f490b2b0b1818
lanxi <lhuang38@wisc.edu>
2014-05-11 12:45:31 -0500

    Finish sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,8 @@ class UsersController < ApplicationController
  def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "Welcome to the Sample App!"
+      sign_in @user
+      flash[:success] = "Welcome to the Trading World!"
       redirect_to @user
     else
       render 'new'
